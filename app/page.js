1e324db1c9bebaa2228cbf052b4bc61fcb095365
Your Name <you@example.com>
2025-01-28 16:55:03 +0100

    Pricing 2nd step

--- app/page.js
+++ app/page.js
@@ -42,20 +42,78 @@ export default function Home() {
           A pricing that adapts to your needs
         </h2>
 
-        <div className="p-8 bg-base-100 w-96 rounded-3xl mx-auto">
+        <div className="p-8 bg-base-100 w-96 rounded-3xl mx-auto space-y-6">
           <div className="flex gap-2 items-baseline">
-           <div className="text 4-l font-black">$19</div> 
+           <div className="text-4xl font-black">$19</div> 
            <div className="uppercase text-sm font-medium opacity-60">/month</div>
-          
+           </div>
 
-        <ul>
-          <li>Collect Customer Feedback</li>
-          <li>Unlimited Boards</li>
-          <li>Admin Fashboard</li>
-          <li>24/7 Support</li>
+        <ul className="space-y-2">
+          <li className="flex gap-2 items-center">
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 16 16" 
+            fill="currentColor" 
+            className="text-green-600 size-4"
+          >
+          <path 
+            fillRule="evenodd" 
+            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" 
+            clipRule="evenodd" 
+          />
+          </svg>
+            Collect Customer Feedback
+            </li>
+            <li className="flex gap-2 items-center">
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 16 16" 
+            fill="currentColor" 
+            className="text-green-600 size-4"
+          >
+          <path 
+            fillRule="evenodd" 
+            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" 
+            clipRule="evenodd" 
+          />
+          </svg>
+            Unlimited Boards
+            </li>
+            <li className="flex gap-2 items-center">
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 16 16" 
+            fill="currentColor" 
+            className="text-green-600 size-4"
+          >
+          <path 
+            fillRule="evenodd" 
+            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" 
+            clipRule="evenodd" 
+          />
+          </svg>
+            Admin Dashboard
+            </li>
+            <li className="flex gap-2 items-center">
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 16 16" 
+            fill="currentColor" 
+            className="text-green-600 size-4"
+          >
+          <path 
+            fillRule="evenodd" 
+            d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" 
+            clipRule="evenodd" 
+          />
+          </svg>
+            24/7 Support
+            </li>  
+          
         </ul>
 
-        </div>
+        <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+
         </div>
     </section>
     </main>
