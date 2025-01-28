cabd9f8d09ec74a96ef0b70bea1e8affd557a518
Your Name <you@example.com>
2025-01-28 15:51:26 +0100

    Pricing Page

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
 
   return (
   <main>
+    {/* HEADER */}
     <section className="bg-base-200">
       <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
         <div className="font-bold">FredApp</div>
@@ -20,6 +21,7 @@ export default function Home() {
         </div>
       </div>
       </section>
+    {/* HERO */}  
       <section className="px-8 text-center py-32 max-w-3xl mx-auto">
       <h1 className="text-4xl font-bold mb-6">
         Collect customer feedback to build better products
@@ -29,6 +31,33 @@ export default function Home() {
       </div>
       <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
       </section>
+
+    {/* PRICING */}
+    <section className="bg-base-200 py-32">
+      <div className="px-8 py-32 max-w-3xl mx-auto"></div>
+        <p className="text-sm uppercase font-medium text-center text-primary mb-4">
+          Pricing
+        </p>
+        <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+          A pricing that adapts to your needs
+        </h2>
+
+        <div className="p-8 bg-base-100 w-96 rounded-3xl mx-auto">
+          <div className="flex gap-2 items-baseline">
+           <div className="text 4-l font-black">$19</div> 
+           <div className="uppercase text-sm font-medium opacity-60">/month</div>
+          
+
+        <ul>
+          <li>Collect Customer Feedback</li>
+          <li>Unlimited Boards</li>
+          <li>Admin Fashboard</li>
+          <li>24/7 Support</li>
+        </ul>
+
+        </div>
+        </div>
+    </section>
     </main>
   );
 }
