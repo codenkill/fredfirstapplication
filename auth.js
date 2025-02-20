59b0a9834133e6141b24001e5d6284f1fa871bf5
Your Name <you@example.com>
2025-02-20 14:53:47 +0100

    Google Login

--- auth.js
+++ auth.js
@@ -1,9 +1,21 @@
 import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./libs/mongo";
+import Resend from "next-auth/providers/resend";
+import Google from "next-auth/providers/google";
 
 const config = {
-    providers: [],
+    providers: [
+        Resend({
+            apiKey: process.env.RESEND_KEY,
+            from: "noreply@resend.calfuel.xyz",
+            name: "Email",
+        }),
+        Google({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+    ],
     adapter: MongoDBAdapter(clientPromise),
 };
 
